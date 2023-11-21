7b800e9d2f1a6a40fb0d9f81b5bcb5a708618423
Andrew R <mydrewdog64@gmail.com>
2023-11-20 19:16:10 -0500

    Karl said ballin

---
+++ sinwave.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+i = 0
+j = 0
+x = []
+y = []
+while i <1000:
+    x.append(i/100)
+    i +=1
+
+while j<1000:
+    y.append(np.sin(x[j]))
+    j+=1
+
+plt.plot(x,y)
+plt.show()
